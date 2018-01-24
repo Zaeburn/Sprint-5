dd837a9c8ea419c1381418edcb05073b6bfe4cab
Zaeburn <Zaeburn@outlook.com>
2018-01-25 01:58:30 +1300

    add solution for 2.3-ChessBoard exercise to eloquent-2.js file

--- eloquent-2.js
+++ eloquent-2.js
@@ -1,13 +1,15 @@
 // Eloquent Javascript Chapter 2 exercise solutions
 
 // 2.1 Looping a Triangle
+
 var triangleOutPut = "#";
 while (triangleOutPut.length <= 7) {
     console.log(triangleOutPut);
     triangleOutPut = triangleOutPut + "#";
 }
 
-// 2.2 Fizz Buzz
+// 2.2 FizzBuzz
+
 for (number = 1; number <= 100; number++) {
     if (number % 3 === 0 && number % 5 === 0) {
       console.log("FizzBuzz");
@@ -18,3 +20,27 @@ for (number = 1; number <= 100; number++) {
     } else
     console.log(number);
   }
+
+// 2.3 ChessBoard
+
+var chessBoard = "";
+var size = 8;
+
+// <outer loop> loop lines
+for (line = 1; line <= size; line++) {
+	// <inner loop> loop characters on line
+	for (character = 1; character <= size; character++)
+    {
+      if ((character + line) % 2)
+        chessBoard = chessBoard + "#";
+      else
+        chessBoard = chessBoard + " ";
+    }
+	// </inner loop>
+  // add a new line
+  chessBoard = chessBoard + "\n"
+}
+// </outer loop>
+
+// log chessBoard variable string to console
+console.log(chessBoard);
